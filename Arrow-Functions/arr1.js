653e4bf8c52ebf2c9de74ea24f1ba1eba67b49a2
Dave <david.halewoodfcc@gmail.com>
2021-02-20 11:11:03 +0000

    adding arrow function png

---
+++ Arrow-Functions/arr1.js
@@ -0,0 +1,9 @@
+function multiplyBy2(input) { return input * 2 };
+
+const multiplyBy2 = (input) => { return input * 2};
+
+const multiplyBy2 = (input) => input * 2;
+
+const multiplyBy2 = input => input * 2;
+
+const output = multiplyBy2(3); // 6
---
+++ Arrow-Functions/readme.md
@@ -0,0 +1,7 @@
+### Arrow functions
+
+
+![arr1-png](arr1.png)
+
+line 1 --> traditional function syntax
+lines 3-7 --> arrow function way of writing the same
